ef0c646305d774438a7b107ce0af89660ee2cc78
Abuchi Kingsley <abuchikingsley76@gmail.com>
2019-04-11 15:33:11 +0100

    chore:setup users table and write user queries

--- server/db.js
+++ server/db.js
@@ -23,7 +23,7 @@ $$;`
 const  createIncidentStatus = `DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'incident_status') THEN
-        create type incident_status AS ENUM ('Pending', 'Under investigation', 'Rejected', 'Resolved');
+        create type incident_status AS ENUM ('Draft', 'Under investigation', 'Rejected', 'Resolved');
     END IF;
 END
 $$;`
@@ -33,23 +33,40 @@ $$;`
  * Create Tables
  */
 const incidentTable = `CREATE TABLE IF NOT EXISTS incidents(
-  incidentId SERIAL  PRIMARY KEY NOT NULL,
+  title VARCHAR(100) NOT NULL,
   createdBy INT NOT NULL,
-  createdOn TIMESTAMPTZ DEFAULT CURRENT_DATE NOT NULL,
+  incidentId SERIAL  PRIMARY KEY NOT NULL,
+  createdOn TIMESTAMP DEFAULT CURRENT_DATE NOT NULL,
   type incident_type NOT NULL,
   location TEXT NOT NULL,
   status incident_status,
-  comment VARCHAR(200) NOT NULL
+  comment VARCHAR(200) NOT NULL,
+  FOREIGN KEY (createdBy) REFERENCES users(id) ON DELETE CASCADE
+
 );`;
 
 
+  const usersTable = `CREATE TABLE IF NOT EXISTS users(
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  other_names TEXT,
+  email TEXT UNIQUE NOT NULL,
+  phone_number TEXT NOT NULL,
+  user_name TEXT UNIQUE,
+  registered TIMESTAMP DEFAULT CURRENT_DATE NOT NULL,
+  password VARCHAR(100),
+  is_admin BOOLEAN DEFAULT false
+
+);`;
 
 
 (async function () {
   try {
     await pool.query(createIncidentType)
     await pool.query(createIncidentStatus)
-    await pool.query(incidentTable);
+    await pool.query(usersTable);
+    await pool.query(incidentTable); 
   } catch (error) {
     console.log(error)
   }
--- server/queries/dbqueries.js
+++ server/queries/dbqueries.js
@@ -12,15 +12,31 @@ const query = {
             values: []
         })
     },
+
+    getIncidentsByType(type) {
+        return ({
+            text: `SELECT * FROM incidents WHERE type = $1`,
+            values: [type]
+        })
+    },
+
+    getIncidentsByStatus(status) {
+        return ({
+            text: `SELECT * FROM incidents WHERE status = 1`,
+            values: [status]
+        })
+    },
+
     createIncident(newIncident) {
         return ({
-            text: `INSERT INTO incidents (createdby, type, location, comment)
-            VALUES($1, $2, $3, $4) RETURNING *`,
+            text: `INSERT INTO incidents (createdby, type, location, comment, status)
+            VALUES($1, $2, $3, $4, $5) RETURNING *`,
             values: [
                 newIncident.createdby,
                 newIncident.type,
                 newIncident.location,
-                newIncident.comment
+                newIncident.comment,
+                newIncident.status
             ]
         })
     },
@@ -42,7 +58,7 @@ const query = {
     },
     updateIncidentStatus(id, incidentUpdate) {
         return ({
-            text:`UPDATE incidents SET
+            text: `UPDATE incidents SET
             status = COALESCE($1, status)
             WHERE incidentid = $2 
             RETURNING *`,
@@ -53,7 +69,6 @@ const query = {
 
         })
     },
-
     deleteIncident(id) {
         return ({
             text: `DELETE FROM incidents WHERE incidentid = $1
@@ -62,9 +77,111 @@ const query = {
 
         })
     },
+
+    getUserById(id) {
+        return ({
+            text: `SELECT * FROM users WHERE id = $1`,
+            values: [id]
+        })
+    },
+
+    regUser(firstName, lastName, otherNames, email, phoneNumber, userName, hashedpassword) {
+        return ({
+            text: `INSERT INTO users (first_name, last_name, other_names, 
+                email, phone_number, user_name, password)
+                VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
+
+            values: [
+                firstName,
+                lastName,
+                otherNames,
+                email,
+                phoneNumber,
+                userName,
+                hashedpassword]
+        })
+    },
+
+    getAllUsers() {
+        return ({
+            text: `SELECT * FROM users`,
+            values: []
+        })
+    },
+
+    getUserByEmail(email) {
+        return ({
+            text: `SELECT * FROM users WHERE email = $1`,
+            values: [email]
+        })
+    },
+
+    getUserByUserName(userName) {
+        return ({
+            text: `SELECT * FROM users WHERE LOWER(user_name) = $1`,
+            values: [userName]
+
+        })
+    },
+
+    getUserByPhone(phoneNumber) {
+        return ({
+            text: `SELECT * FROM users WHERE phone_number = $1`,
+            values: [phoneNumber]
+
+        })
+    },
+
+    updateUserEmail(email, userId) {
+        return ({
+            text: `UPDATE users SET
+            email = COALESCE($1, email) WHERE id = $2`,
+            values: [email, userId]
+        })
+    },
+    updateUserPassword(newHashedPassword, userId) {
+        return ({
+            text: `UPDATE users SET
+            password = COALESCE($1, password) WHERE id = $2`,
+            values: [newHashedPassword, userId]
+        })
+    },
+    updateUser(email, phoneNumber, userName, userId) {
+        return ({
+            text: `UPDATE users SET
+            email = COALESCE($1, email),
+            phone_number = COALESCE($2, phone_number),
+            user_name = COALESCE($4, user_name) 
+            WHERE id = $5 RETURNING *`,
+            values: [email, phoneNumber, userName, userId]
+        })
+    },
+
+    getUserReportsCount(status, userId) {
+        return ({
+            text: `SELECT COUNT(*) FROM incidents WHERE createdby = $1 AND status = $2`,
+            values: [userId, status]
+        })
+    },
+
+    getUserIncidents(userId) {
+        return ({
+            text: `SELECT * FROM incidents WHERE createdby = $1`,
+            values: [userId]
+        })
+    },
+
+  
     getOne(id) {
         return ({
 
+        })
+    },
+
+    deleteUser(userId) {
+        return ({
+            text: `DELETE FROM users WHERE id = $1`,
+            values: [userId]
         })
     }
 
