0327610278a866d55672232a261967316e9840ce
Abuchi Kingsley <abuchikingsley76@gmail.com>
2019-08-19 19:47:57 +0100

    link UI

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import router from './routes/router.js';
 import cors from 'cors'
+import path from 'path'
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,7 @@ app.use(cors())
 app.use(express.json());
 
 app.use(router);
+app.use(express.static(path.join(__dirname, '../UI')))
 
 app.listen(port, () => {
   console.log(`Server Started on port ${port}`);
