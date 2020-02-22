34fa81996842ee20805387eafc0a126e0969b989
Abuchi Kingsley <abuchikingsley76@gmail.com>
2020-02-22 14:08:15 +0100

    Updated info on README

--- server/app.js
+++ server/app.js
@@ -22,3 +22,29 @@ process.on('uncaughtException', function(err) {
 })
 
 export default app;
+
+// var cluster = require('cluster');
+
+// var numCPUs = require('os').cpus().length;
+
+// if (cluster.isMaster) {
+//   for (var i = 0; i < numCPUs; ++i) {
+//     cluster.fork();
+//   }
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log(`worker ${worker.process.pid} died`);
+//     cluster.fork();
+//   });
+// } else {
+//   var http = require('http');
+//   var httpServer = http.createServer(app).listen(httpPort, function () {
+//       console.log('process id local', process.pid)
+//       console.log("http server started at port " + httpPort);
+//   });
+// }
+
+// process.on('uncaughtException', function (err) {
+//   console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
+//   console.error(err.stack)
+//   process.exit(1)
+// })
